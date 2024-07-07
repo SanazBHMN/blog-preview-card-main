67b6745955b11301b0cf9792a80e404e1ff14821
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-07-07 21:49:09 +0330

    Define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: "hsl(47, 88%, 63%)",
+      white: "0, 0%, 100%)",
+      grey: "hsl(0, 0%, 50%)",
+      black: "hsl(0, 0%, 7%)",
+    },
     extend: {},
   },
   plugins: [],
